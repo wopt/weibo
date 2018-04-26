10fdb486ad5d978459c53f76eccc98db047ad902
webspider <513485189@qq.com>
2018-04-26 15:08:23 +0800

    image identifying code

--- login/weibo/cookies.py
+++ login/weibo/cookies.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from os import listdir
 from os.path import abspath, dirname
+from selenium import webdriver
 
 TEMPLATES_FOLDER = dirname(abspath(__file__)) + '/templates/'
 
@@ -80,7 +81,7 @@ class WeiboCookies():
         :return: 截图对象
         """
         screenshot = self.browser.get_screenshot_as_png()
-        screenshot = Image.open(BytesIO(screenshot))
+        screenshot = Image.open(BytesIO(screenshot)).convert('L')
         return screenshot
     
     def get_image(self, name='captcha.png'):
@@ -106,9 +107,8 @@ class WeiboCookies():
         # 取两个图片的像素点
         pixel1 = image1.load()[x, y]
         pixel2 = image2.load()[x, y]
-        threshold = 20
-        if abs(pixel1[0] - pixel2[0]) < threshold and abs(pixel1[1] - pixel2[1]) < threshold and abs(
-                pixel1[2] - pixel2[2]) < threshold:
+        threshold = 18
+        if abs(pixel1 - pixel2) < threshold:
             return True
         else:
             return False
@@ -121,7 +121,7 @@ class WeiboCookies():
         :return:
         """
         # 相似度阈值
-        threshold = 0.99
+        threshold = 0.989
         count = 0
         for x in range(image.width):
             for y in range(image.height):
@@ -131,8 +131,8 @@ class WeiboCookies():
         result = float(count) / (image.width * image.height)
         if result > threshold:
             print('成功匹配')
-            return True
-        return False
+            return 1.0
+        return result
     
     def detect_image(self, image):
         """
@@ -140,14 +140,25 @@ class WeiboCookies():
         :param image: 图片
         :return: 拖动顺序
         """
+        threshold = 0.98
+        result_dict = {}
         for template_name in listdir(TEMPLATES_FOLDER):
             print('正在匹配', template_name)
-            template = Image.open(TEMPLATES_FOLDER + template_name)
-            if self.same_image(image, template):
-                # 返回顺序
-                numbers = [int(number) for number in list(template_name.split('.')[0])]
+            template = Image.open(TEMPLATES_FOLDER + template_name).convert('L')
+            result = self.same_image(image, template)
+            name = template_name.split('.')[0]
+            if result != 1.0:
+                result_dict[name] = result
+            else:
+                numbers = [int(number) for number in list(name)]
                 print('拖动顺序', numbers)
                 return numbers
+
+        result_list = sorted(result_dict.items(), key=lambda d: d[1], reverse=True)
+        if result_list[0][1] > threshold:
+            numbers = [int(number) for number in list(result_list[0][0])]
+            print('拖动顺序', numbers)
+            return numbers
     
     def move(self, numbers):
         """
@@ -212,6 +223,10 @@ class WeiboCookies():
             }
         # 获取验证码图片
         image = self.get_image('captcha.png')
+        # my code
+        image.save('captcha.png')
+        image.show()
+
         numbers = self.detect_image(image)
         self.move(numbers)
         if self.login_successfully():
@@ -228,5 +243,10 @@ class WeiboCookies():
 
 
 if __name__ == '__main__':
-    result = WeiboCookies('14773427930', 'x6pybpakq1').main()
+    chrome_opt = webdriver.ChromeOptions()
+    chrome_opt.add_argument('--headless')
+    chrome_opt.add_argument('--disable-gpu')
+    browser = webdriver.Chrome(chrome_options=chrome_opt)
+
+    result = WeiboCookies('18890304051', 'meadzh093', browser).main()
     print(result)
